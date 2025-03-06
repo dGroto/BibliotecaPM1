356bd4959faa043abb53ed8b9aae9c734cff8c51
neulandp <neulandp074@gmail.com>
2025-03-06 20:05:11 -0300

    Documentação Main

--- src/main/java/org/principal/Main.java
+++ src/main/java/org/principal/Main.java
@@ -4,44 +4,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Biblioteca biblioteca = new Biblioteca();
-        boolean continuar = true;
+        Scanner sc = new Scanner(System.in); //Permite ler dados inseridos pelo usuário através do teclado
+        Biblioteca biblioteca = new Biblioteca(); //Criado uma instância de um objeto da Classe chamada "Biblioteca"
+        boolean continuar = true; //Variável inicializada com valor booleano para rodar o "while"
 
-        while (continuar) {
+        while (continuar) { //Continuará o loop se a variável for true
             System.out.println("\n\t1. Adicionar Livro"
            +  "\n\t2. Cadastrar Leitor"
            + "\n\t3. Registrar Empréstimo"
            + "\n\t4. Listar Empréstimos"
            + "\n\t5. Listar Leitores"
            + "\n\t6. Sair"
-           + "\n Escolha uma opção: ");
-            int escolha = sc.nextInt();
-            sc.nextLine(); //Consumir a linha em branco após o número
+           + "\n Escolha uma opção: "); //Imprime no console
+            int escolha = sc.nextInt(); //Armazena na variável a opção digitada pelo usuário
+            sc.nextLine(); //Lê uma linha de texto inserida pelo usuário e retorna, mesmo tendo espaços
 
-            switch (escolha) {
+            switch (escolha) { //Executa o bloco de código de acordo com a escolha do usuário
                 case 1:
-                    biblioteca.addLivro();
+                    biblioteca.addLivro(); //Chama o metodo para adicionar um livro
                     break;
                 case 2:
-                    biblioteca.cadastrarLeitor();
+                    biblioteca.cadastrarLeitor(); //Chama o metodo para cadastrar um leitor
                     break;
                 case 3:
-                    biblioteca.registrarEmprestimo();
+                    biblioteca.registrarEmprestimo(); //Chama o metodo para registrar um empréstimo de livro
                     break;
                 case 4:
-                    biblioteca.listarEmprestimos();
+                    biblioteca.listarEmprestimos(); //Chama o metodo para mostrar todos os empréstimos registrados
                     break;
                 case 5:
-                    biblioteca.listarLeitores();
+                    biblioteca.listarLeitores(); //Chama o metodo para listar todos os leitores cadastrados
                     break;
                 case 6:
-                    continuar = false;
+                    continuar = false; //O loop continua até o usuário escolher a opção 6 para sair, pois a variável é definida como "false"
                     break;
                 default:
-                    System.out.println("Opção inválida.");
+                    System.out.println("Opção inválida."); //Imprime no console
+                    break;
             }
         }
-        sc.close();
+        sc.close(); //Fecha o Scanner após o uso
     }
 }
--- src/main/java/system/Livros.java
+++ src/main/java/system/Livros.java
@@ -82,7 +82,6 @@ public class Livros {
 
     }
 
-    @Override
     public String toString() {
         return "\tTitulo: " + titulo + "\n\tAutor: " + autor + "\n\tGenero: " + genero
         + "\n\tAno de Publicacao: " + anoPublicacao + "\n\tDisponivel: " + disponivel + "\n----------------------------";
