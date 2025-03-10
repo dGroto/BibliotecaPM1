416591d0ec9b975f3df2868e64fc10c394783208
neulandp <neulandp074@gmail.com>
2025-03-10 20:26:28 -0300

    Documentação da Classe Livros

--- src/main/java/system/Livros.java
+++ src/main/java/system/Livros.java
@@ -1,14 +1,13 @@
-package system;
-//teste
-public class Livros {
+package system; //Pacote criado
 
+public class Livros { //Classe Livros com atributos privados
     private String titulo;
     private String autor;
     private String genero;
     protected String anoPublicacao;
-    protected Status status;
+    protected Status status; //Status do livro, se está disponível ou indisponível para empréstimo
 
-    //CONSTRUTOR
+    //Construtor da classe Livros, cria um objeto com todas as informações
     public Livros (String titulo, String autor, String genero, String anoPublicacao, Status status) {
         this.titulo = titulo;
         this.autor = autor;
@@ -17,74 +16,63 @@ public class Livros {
         this.status = status;
     }
 
-    public Livros(){
+    public Livros(){ //Construtor vazio
     }
 
-    //GET E SET
-
-    //TITULO
-    //GETTERS RETORNAM INFORMAÇOES
-    public String getTitulo(){
+    //Metodos Getters e Setters
+    public String getTitulo(){ //Get para o título do livro
         return titulo;
     }
-    //SETTERS ATRIBUEM
-    public void setTitulo(String titulo){
+
+    public void setTitulo(String titulo){ //Set para atribuir valor
         this.titulo = titulo;
     }
 
-    //AUTOR
-    public String getAutor(){
+    public String getAutor(){ //Get e Set para o autor do livro
         return autor;
     }
     public void setAutor (String autor){
         this.autor = autor;
     }
 
-    //GENERO
-    public String getGenero(){
+    public String getGenero(){ //Get e Set para o gênero do livro
         return genero;
     }
     public void setGenero(String genero){
         this.genero = genero;
     }
 
-    //ANO DE  PUBLICAÇÃO
-    public String getAnoPublicacao(){
+    public String getAnoPublicacao(){ //Get e Set para o ano de publicação do livro
         return anoPublicacao;
     }
     public void setAnoPublicacao(String anoPublicacao){
         this.anoPublicacao = anoPublicacao;
     }
 
-    //STATUS
-    public void setStatus(Status status){
+    public void setStatus(Status status){ //Get e Set para status do livro
         this.status = status;
     }
     public Status getStatus(){
         return status;
     }
 
-    //  METODOS/FUNÇÕES
-    public void exibirInfos(){
+    public void exibirInfos(){ //Metodo para mostrar as informações do livro
         System.out.println("T\n\titulo: " + titulo
                 + "\n\tAutor: " + autor
                 + "\n\tGênero: " + genero
-                + "\n\tAno de Publicação: " + anoPublicacao);
+                + "\n\tAno de Publicação: " + anoPublicacao); //Imprime no console
     }
 
-
-    public void emprestar(){
-
-    }
-
-    public void devolver(){
+    public void emprestar(){ //Metodo para emprestar um livro
 
     }
 
-    public String toString() {
+    public void devolver(){ //Metodo para devolver um livro
+
+    }
+
+    public String toString() { //Metodo toString para retornar informações do livro como uma string formatada
         return "\tTitulo: " + titulo + "\n\tAutor: " + autor + "\n\tGenero: " + genero
                 + "\n\tAno de Publicacao: " + anoPublicacao + "\n\t" + status + "\n----------------------------";
     }
-
-
 }
