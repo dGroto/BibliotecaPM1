595bd873a03f4335b30063768bc08233b0aa51fc
neulandp <neulandp074@gmail.com>
2025-03-10 20:32:40 -0300

    Documentação da Classe Leitor

--- src/main/java/system/Leitor.java
+++ src/main/java/system/Leitor.java
@@ -1,25 +1,24 @@
-package system;
+package system; //Pacote criado
 import java.util.List;
 import java.util.ArrayList;
 
-public class Leitor {
-
+public class Leitor { //Classe leitor com atributos privados
   private String nome;
-  private String cpf; // Adicionei um CPF para o cadastro
-  private List<Emprestimo> emprestimos;
+  private String cpf;
+  private List<Emprestimo> emprestimos; //Lista de empréstimos realizados pelo leitor
 
-  //Construtor
+  //Construtor que inicializa o nome, CPF e a lista de empréstimos
   public Leitor(String nome, String cpf) {
     this.nome = nome;
     this.cpf = cpf;
+    this.emprestimos = new ArrayList<>(); //Inicializa a lista de empréstimos como uma nova ArrayList
+  }
+
+  public Leitor() { //onstrutor vazio que também inicializa a lista de empréstimos
     this.emprestimos = new ArrayList<>();
   }
 
-  public Leitor() {
-    this.emprestimos = new ArrayList<>();
-  }
-
-  //Getters e Setters
+  //Getter e setter para o nome do leitor
   public String getNome() {
     return nome;
   }
@@ -28,6 +27,7 @@ public class Leitor {
     this.nome = nome;
   }
 
+  //Getter e setter para o CPF do leitor
   public String getCpf() {
     return cpf;
   }
@@ -36,6 +36,7 @@ public class Leitor {
     this.cpf = cpf;
   }
 
+  //Getter e setter para a lista de empréstimos do leitor
   public List<Emprestimo> getEmprestimos() {
     return emprestimos;
   }
@@ -44,17 +45,18 @@ public class Leitor {
     this.emprestimos = emprestimos;
   }
 
-  //Método para atualizar cadastro
-  public void atualizarCadastro(String nome, String cpf) {
+  //Metodo para atualizar os dados do cadastro do leitor
+  public void atualizarCadastro(String nome, String cpf) { //Atualiza o nome e CPF do leitor
     setNome(nome);
     setCpf(cpf);
   }
 
-  //Método para solicitar empréstimo
+  //Metodo para solicitar um empréstimo de livro
   public void solicitarEmprestimo(Emprestimo emprestimo) {
-    emprestimos.add(emprestimo);
+    emprestimos.add(emprestimo); //Adiciona um empréstimo na lista de empréstimos do leitor
   }
 
+  //Metodo toString para retornar como uma string
   @Override
   public String toString() {
     return "\tNome: " + nome + "\n\tCPF: " + cpf + "\n----------------------------";
