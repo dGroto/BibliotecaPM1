0c3d008f5fd0ad652d52bfebb213285f0d1ee821
neulandp <neulandp074@gmail.com>
2025-03-10 21:05:22 -0300

    Documentação da Interface

--- src/main/java/system/IBiblioteca.java
+++ src/main/java/system/IBiblioteca.java
@@ -1,12 +1,11 @@
-package system;
+package system; //Pacote criado
 
-public interface IBiblioteca {
-
+public interface IBiblioteca { //Definição da interface
+    //Métodos definidos na interface
     public void addLivro();
     public void cadastrarLeitor();
     public void listarEmprestimos();
     public void registrarEmprestimo();
     public void listarLeitores();
     public void listarLivros();
-
 }
