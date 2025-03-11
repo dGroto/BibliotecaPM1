fa17e14300ee0c464a405e6dac83ed68f41e9b1e
neulandp <neulandp074@gmail.com>
2025-03-10 21:10:40 -0300

    Documentação da Classe Emprestimo

--- src/main/java/system/Emprestimo.java
+++ src/main/java/system/Emprestimo.java
@@ -1,65 +1,54 @@
-package system;
+package system; //Pacote criado
 
-import java.time.LocalDate;
-
-public class Emprestimo extends Livros {
-
+public class Emprestimo extends Livros { //Classe Emprestimo herda da Classe Livros
     private Leitor leitor;
     private Livros livro;
     protected Status status;
 
-    // CONSTRUTOR
-    public Emprestimo(Leitor leitor, Livros livro, Status status ) {
+    //Construtor que inicializa os atributos da Classe Emprestimo
+    public Emprestimo(Leitor leitor, Livros livro, Status status) {
         this.leitor = leitor;
         this.livro = livro;
         this.status = status;
     }
 
-    // METODOS
-    public void registrarEmprestimo() {
-
-
-        if (livro.getStatus() == Status.DISPONIVEL) {
+    public void registrarEmprestimo() { //Metodo que registra o empréstimo do livro
+        if(livro.getStatus() == Status.DISPONIVEL) { //Verifica se o livro está disponível para empréstimo
             livro.setStatus(Status.INDISPONIVEL); //Marcar o livro como emprestado
-            System.out.println("Emprestimo registrado com sucesso!");
-        } else {
+            System.out.println("Emprestimo registrado com sucesso!"); //Imprime no console
+        }else{ //Caso o livro não esteja disponível, mostra a mensagem de erro no console
             System.out.println("O livro não está disponível.");
         }
     }
 
-    public void registrarDevolucao() {
+    public void registrarDevolucao() { //Metodo que registra a devolução do livro
         livro.setStatus(Status.DISPONIVEL); //Marcar o livro como devolvido
-        System.out.println("Devolução registrada com sucesso!");
+        System.out.println("Devolução registrada com sucesso!"); //Imprime no console
     }
 
-
-    public Leitor getLeitor() {
-
+    public Leitor getLeitor() { //Getter para o leitor que realizou o empréstimo
         return leitor;
     }
 
-    public void setLeitor(Leitor leitor) {
-
+    public void setLeitor(Leitor leitor) { //Setter para o leitor que realizou o empréstimo
         this.leitor = leitor;
     }
 
-    public Livros getLivro() {
-
+    public Livros getLivro() { //Getter para o livro que foi emprestado
         return livro;
     }
 
-    public void setLivro(Livros livro) {
-
+    public void setLivro(Livros livro) { //Setter para o livro que foi emprestado
         this.livro = livro;
     }
 
     @Override
-    public Status getStatus() {
+    public Status getStatus() { //Getter para o status do empréstimo
         return status;
     }
 
     @Override
-    public void setStatus(Status status) {
+    public void setStatus(Status status) { //Setter para o status do empréstimo
         this.status = status;
     }
 }
